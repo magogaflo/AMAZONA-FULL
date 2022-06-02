9dd01cd8fe6edcc17fb757918eb2947a41d12bc0
magogaflo <patt8i@hotmail.com>
2022-06-02 00:14:49 -0300

    subida4

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,30 @@
-
+import Data from './Data';
 
 function App() {
   return (
     <div className="App">
-      <header >
-       <a href="/">amazona</a>
+      <header>
+        <a href="/">amazona</a>
       </header>
       <main>
-        Lista de productos
+        <h1>Caracteristicas de los Productos</h1>
+        <div className="products">
+          {Data.products.map((product) => (
+            <div className="product" key={product.slug}>
+              <a href={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
+
+              <div className="product-info">
+                <a href={`/product/${product.slug}`}>
+                  <p>{product.name}</p>
+                </a>
+                <p><strong>{product.price}</strong></p>
+                <button>Add to Cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
       </main>
     </div>
   );
---
+++ frontend/src/Data.js
@@ -0,0 +1,54 @@
+const data ={
+    products: [
+        {
+            name:'Nike Slim shirt',
+            slug:'nike-slim-shirt',
+            category:'Shirts',
+            image:'/images/p1.jpg',
+            price:120,
+            countInStock:20,
+            brand:'Nike',
+            rating:4.5,
+            numReviews:10,
+            description:'high quality shirt'
+        },
+        {
+            name:'Adidas Fit Shirt',
+            slug:'adidas-fit-shirt',
+            category:'Shirts',
+            image:'/images/p2.jpg',
+            price:220,
+            countInStock:20,
+            brand:'Adidas',
+            rating:4.2,
+            numReviews:10,
+            description:'high quality products'     
+        },
+        {
+            name:'Nike Slim Pant',
+            slug:'nike-slim-Pant',
+            category:'Pants',
+            image:'/images/p3.jpg',
+            price:400,
+            countInStock:15,
+            brand:'Nike',
+            rating:4.0,
+            numReviews:14,
+            description:'high quality products'
+        },
+        {
+            name:'Adidas Fit Pant',
+            slug:'adidas-fit-pant',
+            category:'Pants',
+            image:'/images/p4.jpg',
+            price:300,
+            countInStock:25,
+            brand:'Adidas',
+            rating:4.5,
+            numReviews:15,
+            description:'high quality products'
+        }
+    ],
+};
+
+export default data;
